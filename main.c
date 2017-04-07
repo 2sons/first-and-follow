b5bb38cc7eeff5529b2af66abe89c09f6f053a8b
NihalSh <nihal.sh.1995@gmail.com>
2017-04-07 15:07:36 +0530

    initial commit, getInput function implementation (partial)

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+	return 0;
+}
+
+int getInput()
+{
+	int i;
+	int numVariables;
+	int numTerminals;
+	printf("Variables: ");
+	scanf(" %d", &numVariables);
+	for (i = 0; i < numVariables; i++) {
+
+	}
+	printf("Terminals: ");
+	scanf(" %d", &numTerminals);
+	for (i = 0; i < numTerminals; i++) {
+
+	}
+	return 0;
+}
